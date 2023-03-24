ed0731ba22bd35df572ce69ae09aae1b486534cc
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-24 09:09:04 -0400

    Turns out FireFox doesn't support Manifest v3 yet

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,15 @@
+async function main() {
+  // await browser.scripting.registerContentScripts([
+  //   {
+  //     id: "content-main",
+  //     js: ["content_scripts/index.js"],
+  //     matches: ["<all_urls>"],
+  //   },
+  // ]);
+}
+
+main();
+
 chrome.runtime.onInstalled.addListener(function ({ reason }) {
   console.log("lightspeed installed");
 });
